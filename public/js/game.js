2f717b791ee79df786c4c2b69d38fbd22214b472
andrewlau <andrewlau624@gmail.com>
2025-09-29 02:34:11 -0700

    Fixed buttons

--- public/js/game.js
+++ public/js/game.js
@@ -5,6 +5,19 @@ if (!username) {
   window.location.href = "index.html";
 }
 
+const buttons = document.querySelectorAll("#choices button");
+let selected;
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    buttons.forEach((b) => b.classList.remove("selected"));
+    btn.classList.add("selected");
+    selected = btn.dataset.choice;
+
+    socket.emit("makeChoice", { choice: selected });
+  });
+});
+
 socket.emit("joinGame", { username });
 
 socket.on("roomJoined", ({ roomId }) => {
@@ -17,6 +30,11 @@ socket.on("roomFull", ({ players }) => {
   document.getElementById(
     "status"
   ).innerText = `Room ready! Players: ${players.join(" vs ")}`;
+
+  buttons.forEach((btn) => {
+    buttons.forEach((b) => b.classList.remove("selected"));
+    selected = null;
+  });
 });
 
 socket.on("roomLeft", () => {
@@ -25,36 +43,39 @@ socket.on("roomLeft", () => {
   ).innerText = `The other player left the game. Waiting for another player...`;
 });
 
-const buttons = document.querySelectorAll("#choices button");
-let selected;
-
-buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    buttons.forEach(b => b.classList.remove('selected'));
-    btn.classList.add('selected');
-    selected = btn.dataset.choice;
-
-    socket.emit("makeChoice", { choice: selected });
-  });
-});
-
 socket.on("gameResult", ({ p1, p2 }) => {
-    const player = p1.id === socket.id ? p1 : p2;
-    const opponent = p1.id === socket.id ? p2 : p1;
+  const player = p1.id === socket.id ? p1 : p2;
+  const opponent = p1.id === socket.id ? p2 : p1;
 
-    const choiceMap = {
-        1: "Rock",
-        0: "Paper",
-        3: "Scissors"
-    };
+  const choiceMap = {
+    1: "Rock",
+    0: "Paper",
+    3: "Scissors",
+  };
 
-    const diff = (player.choice - opponent.choice);
+  const diff = player.choice - opponent.choice;
 
-    if (diff === 0) {
-        document.getElementById("status").innerHTML = `It's a <b>tie</b>! You both chose ${choiceMap[player.choice]}. <br> Select another hand to play again.`;
-    } else if (diff > 0 && diff !== 2 || diff === -2) {
-        document.getElementById("status").innerHTML = `You <b>lose</b>! You chose <b>${choiceMap[player.choice]}</b> and ${opponent.username} chose <b>${choiceMap[opponent.choice]}</b>. <br><br> Select another hand to play again.`;
-    } else {
-        document.getElementById("status").innerHTML = `You <b>lose</b>! You chose <b>${choiceMap[player.choice]}</b> and ${opponent.username} chose <b>${choiceMap[opponent.choice]}</b>. <br><br> Select another hand to play again.`;
-    }
+  if (diff === 0) {
+    document.getElementById(
+      "status"
+    ).innerHTML = `It's a <b>tie</b>! You both chose ${
+      choiceMap[player.choice]
+    }. <br> Select another hand to play again.`;
+  } else if ((diff > 0 && diff !== 2) || diff === -2) {
+    document.getElementById(
+      "status"
+    ).innerHTML = `You <b>lose</b>! You chose <b>${
+      choiceMap[player.choice]
+    }</b> and ${opponent.username} chose <b>${
+      choiceMap[opponent.choice]
+    }</b>. <br><br> Select another hand to play again.`;
+  } else {
+    document.getElementById(
+      "status"
+    ).innerHTML = `You <b>lose</b>! You chose <b>${
+      choiceMap[player.choice]
+    }</b> and ${opponent.username} chose <b>${
+      choiceMap[opponent.choice]
+    }</b>. <br><br> Select another hand to play again.`;
+  }
 });
